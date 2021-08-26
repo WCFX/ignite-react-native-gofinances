bbf96214f79df05391ca4140ab405466bbad6e6b
wcfx <wcfx.dev@gmail.com>
2021-08-26 20:49:25 -0300

    feat: index with all export config

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as HighlightCard } from './HighlightCard';
+export { default as TransactionCard } from './TransactionCard';
+export { default as Input } from './Form/Input';
+export { default as InputForm } from './Form/InputForm';
+export { default as Button } from './Form/Button';
+export { default as TransactionButton } from './Form/TransactionButton';
+export { default as CategorySelectButton } from './Form/CategorySelectButton';
+export { default as SignInSocialButton } from './SignInSocialButton';
