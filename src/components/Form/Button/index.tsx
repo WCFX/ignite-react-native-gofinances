cd8a7cc71c22234f6a7829bd008c094c9db3ac49
wcfx <wcfx.dev@gmail.com>
2021-08-26 18:58:16 -0300

    feat: button form

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { RectButtonProps } from 'react-native-gesture-handler';
+import { TouchableOpacityProps } from 'react-native';
 
-import { Container, Title } from './styles';
+import * as S from './styles';
 
-interface Props extends RectButtonProps{
+interface Props extends TouchableOpacityProps {
   title: string;
-  onPress: () => void;
 }
 
-export function Button({
-  title,
-  onPress,
-  ...rest
-}: Props){
+const Button = ({ title, ...rest }: Props) => {
   return (
-    <Container onPress={onPress} {...rest}>
-      <Title>
-        { title }
-      </Title>
-    </Container>
+    <S.Container {...rest}>
+      <S.Title>{title}</S.Title>
+    </S.Container>
   );
-}
+};
+
+export default Button;
