096617fe00c04268518207baf149f1625b383708
wcfx <wcfx.dev@gmail.com>
2021-08-26 20:54:33 -0300

    feat: index with all export config

---
+++ src/screens/index.ts
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Register } from './Register';
+export { default as CategorySelect } from './CategorySelect';
+export { default as Resume } from './Resume';
+export { default as SignIn } from './SignIn';
