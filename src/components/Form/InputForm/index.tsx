7eeb0240623f57b6a1bb9a8937a2a5250750b71f
wcfx <wcfx.dev@gmail.com>
2021-08-26 18:59:39 -0300

    feat: input with validation

--- src/components/Form/InputForm/index.tsx
+++ src/components/Form/InputForm/index.tsx
@@ -1,37 +1,31 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
+
 import { Control, Controller } from 'react-hook-form';
 
-import { Input } from '../Input';
+import * as S from './styles';
 
-import { Container, Error } from './styles';
+import Input from '../Input';
 
-interface Props extends TextInputProps {
+export interface Props extends TextInputProps {
   control: Control;
   name: string;
   error: string;
 }
 
-export function InputForm({
-  control,
-  name,
-  error,
-  ...rest
-}: Props){
-  return(
-    <Container>
+const InputForm = ({ error, control, name, ...rest }: Props) => {
+  return (
+    <S.Container>
       <Controller
         control={control}
-        render={({ field: { onChange, value }}) => (
-          <Input
-            onChangeText={onChange}
-            value={value}
-            {...rest}
-          />
-        )}
         name={name}
+        render={({ field: { onChange, value } }) => (
+          <Input onChangeText={onChange} value={value} {...rest} />
+        )}
       />
-      {error && <Error>{ error }</Error>}
-    </Container>
+      {error && <S.Error>{error}</S.Error>}
+    </S.Container>
   );
-}
+};
+
+export default InputForm;
